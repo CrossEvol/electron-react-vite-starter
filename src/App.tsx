b7085fc874c44b9d527f32bb51074bad01340a54
创圣大天使Evol <296662402@qq.com>
2023-08-09 00:40:01 +0800

    0.0.0.1 integrate with the tailwindcss and test sucessfully

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,40 @@
-import * as React from 'react';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
-import ProTip from './ProTip';
+import * as React from 'react'
+import Container from '@mui/material/Container'
+import Typography from '@mui/material/Typography'
+import Box from '@mui/material/Box'
+import Link from '@mui/material/Link'
+import ProTip from './ProTip'
 
-console.log('[App.tsx]', `Hello world from Electron ${process.versions.electron}!`)
+console.log(
+    '[App.tsx]',
+    `Hello world from Electron ${process.versions.electron}!`
+)
 
 function Copyright() {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}.
-    </Typography>
-  );
+    return (
+        <Typography variant='body2' color='text.secondary' align='center'>
+            {'Copyright © '}
+            <Link color='inherit' href='https://mui.com/'>
+                Your Website
+            </Link>{' '}
+            {new Date().getFullYear()}.
+        </Typography>
+    )
 }
 
 export default function App() {
-  return (
-    <Container maxWidth="sm">
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Material UI Vite.js example in TypeScript
-        </Typography>
-        <ProTip />
-        <Copyright />
-      </Box>
-    </Container>
-  );
+    return (
+        <Container maxWidth='sm'>
+            <Box sx={{ my: 4 }}>
+                <Typography variant='h4' component='h1' gutterBottom>
+                    Material UI Vite.js example in TypeScript
+                </Typography>
+                <ProTip />
+                <Copyright />
+                <div className='bg-purple-600 h-20 w-52 text-center rounded-lg'>
+                    123
+                </div>
+            </Box>
+        </Container>
+    )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,18 @@
-import { ThemeProvider } from '@emotion/react';
-import { CssBaseline } from '@mui/material';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './samples/node-api';
-import theme from './theme';
+import { ThemeProvider } from '@emotion/react'
+import { CssBaseline } from '@mui/material'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './input.css'
+import './samples/node-api'
+import theme from './theme'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>,
-);
+    <React.StrictMode>
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <App />
+        </ThemeProvider>
+    </React.StrictMode>
+)
 postMessage({ payload: 'removeLoading' }, '*')
