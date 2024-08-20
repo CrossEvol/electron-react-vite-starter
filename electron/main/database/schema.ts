e1198fa6af6c3b171faf64bedfba384974da5173
创圣大天使Evol <296662402@qq.com>
2024-08-20 19:22:08 +0800

    stuck in the worker path error should import the logger system to check the error

--- electron/main/database/schema.ts
+++ electron/main/database/schema.ts
@@ -1,14 +1,14 @@
 import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
-export const users = sqliteTable('users', {
+export const UsersTable = sqliteTable('users', {
 	id: integer('id').primaryKey(), // 'id' is the column name
 	fullName: text('full_name'),
 });
 
-export const projects = sqliteTable('projects', {
+export const ProjectsTable = sqliteTable('projects', {
 	id: integer('id').primaryKey(), // 'id' is the column name
 	name: text('name'),
 	ownerId: integer('owner_id')
 		.notNull()
-		.references(() => users.id),
+		.references(() => UsersTable.id),
 });
