8b973149c29b5ff7084e32bb507e978d7c83148d
创圣大天使Evol <296662402@qq.com>
2024-08-20 20:02:21 +0800

    pnpm run lint --fix

--- src/renderer.d.ts
+++ src/renderer.d.ts
@@ -1,13 +1,12 @@
-
 export interface IElectronAPI {
-    openFile: () => Promise<string>
-    sendMessage: (message) => Promise<void>
-    sendTwoWayMessage: (message) => Promise<string>
-    updatePort: () => Promise<{ port: number }>
+  openFile: () => Promise<string>
+  sendMessage: (message) => Promise<void>
+  sendTwoWayMessage: (message) => Promise<string>
+  updatePort: () => Promise<{ port: number }>
 }
 
 declare global {
-    interface Window {
-        electronAPI: IElectronAPI
-    }
+  interface Window {
+    electronAPI: IElectronAPI
+  }
 }
--- src/views/home.tsx
+++ src/views/home.tsx
@@ -8,47 +8,47 @@ import ProTip from '../ProTip'
 import fetchClient from '@/utils/fetch.client'
 
 function Copyright() {
-    return (
-        <Typography variant='body2' color='text.secondary' align='center'>
-            {'Copyright © '}
-            <Link color='inherit' href='https://mui.com/'>
-                Your Website
-            </Link>{' '}
-            {new Date().getFullYear()}.
-        </Typography>
-    )
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}.
+    </Typography>
+  )
 }
 
 export default function Home() {
-    React.useEffect(() => {
-        handleUpdatePort()
-    }, [])
+  React.useEffect(() => {
+    handleUpdatePort()
+  }, [])
 
-    const handleUpdatePort = async () => {
-        const { port } = await window.electronAPI.updatePort()
-        localStorage.setItem('port', port.toString())
-    }
+  const handleUpdatePort = async () => {
+    const { port } = await window.electronAPI.updatePort()
+    localStorage.setItem('port', port.toString())
+  }
 
-    const handleHttpRequest = async () => {
-        const res = await fetchClient.get(
-            `http://localhost:${localStorage.getItem('port')}/users`
-        )
-        console.log(res)
-    }
-
-    return (
-        <Container maxWidth='sm'>
-            <Box sx={{ my: 4 }}>
-                <Typography variant='h4' component='h1' sx={{ mb: 2 }}>
-                    Material UI Vite.js example in TypeScript
-                </Typography>
-                <Button variant='contained' onClick={handleHttpRequest}>
-                    Test HttpRequest
-                </Button>
-                <h1 className='text-3xl font-bold underline'>Hello world!</h1>
-                <ProTip />
-                <Copyright />
-            </Box>
-        </Container>
+  const handleHttpRequest = async () => {
+    const res = await fetchClient.get(
+      `http://localhost:${localStorage.getItem('port')}/users`
     )
+    console.log(res)
+  }
+
+  return (
+    <Container maxWidth="sm">
+      <Box sx={{ my: 4 }}>
+        <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
+          Material UI Vite.js example in TypeScript
+        </Typography>
+        <Button variant="contained" onClick={handleHttpRequest}>
+          Test HttpRequest
+        </Button>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <ProTip />
+        <Copyright />
+      </Box>
+    </Container>
+  )
 }
