8f33b02d48abb68fd1860d6f9732dec2a84a68d7
创圣大天使Evol <296662402@qq.com>
2024-08-22 12:27:14 +0800

    update README.MD and prepare pnpm-workspace

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,8 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   test: {
+    exclude: ['**/node_modules/**', '**/dist/**', '**/coverage/**', 'e2e/**'],
     environment: 'jsdom',
-    setupFiles: ['./vitest.setup.ts'],
-  },
+    setupFiles: ['./vitest.setup.ts']
+  }
 })
