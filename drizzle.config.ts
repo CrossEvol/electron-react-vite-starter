2a74c0c365a0a51d6f5b23bfdfcbab8aacfc53fd
CrossEvol <296662402@qq.com>
2025-07-17 15:10:54 +0800

    build: upgrade drizzle-orm && drizzle-kit

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,11 @@
-import type { Config } from 'drizzle-kit';
+import type { Config } from 'drizzle-kit'
 
 export default {
-	schema: './electron/main/database/schema.ts',
-	out: './drizzle',
-	driver: 'better-sqlite',
-	dbCredentials: {
-		url: './sqlite.db',
-	},
-} satisfies Config;
+  schema: './electron/main/database/schema.ts',
+  out: './drizzle',
+  dialect: 'sqlite',
+  driver: 'durable-sqlite',
+  dbCredentials: {
+    url: './sqlite.db'
+  }
+} satisfies Config
